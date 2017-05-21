46741f1ac9da7b81945d3adde758d3b367ec7d9e
Brendan Walker <brenny_walker@hotmail.com>
2017-05-21 11:22:01 -0700

    Adds final step to game sequence where players can see each others rendition of the randomly chosen shape

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -9,12 +9,24 @@ class GameOver extends React.Component {
     super();
     this.state = {
       lines: new Immutable.List(),
-      shape: new Immutable.List()
+      shape: new Immutable.List(),
+      drawingOne: new Immutable.List(),
+      drawingTwo: new Immutable.List()
     };
   }
 
   componentDidMount() {
-    this.setState({lines: this.props.lines, shape: this.props.shape});
+    this.setState({
+      lines: this.props.lines, 
+      shape: this.props.shape,
+      drawingOne: this.props.drawingOne,
+      drawingTwo: this.props.drawingTwo
+    });
+  }
+
+  // TODO reset game and all the data
+  gameReset() {
+    console.log('reset!');
   }
 
   render() {
@@ -26,12 +38,17 @@ class GameOver extends React.Component {
           <Drawing lines={this.state.shape} />
         </div>
         <h3 className='prompt'>After...</h3>
-        <div className='drawArea' ref='drawArea'>
-          <Drawing lines={this.state.lines} />
-        </div>
+        <span>
+          <div className='drawArea' ref='drawArea'>
+            <Drawing lines={this.state.drawingOne} />
+          </div>
+          <div className='drawArea' ref='drawArea'>
+            <Drawing lines={this.state.drawingTwo} />
+          </div>
+        </span>
         <span className='button-container'>
           <Link to='/home'>
-            <RaisedButton className='draw-options' primary={true} >
+            <RaisedButton className='draw-options' primary={true} onTouchTap={this.gameReset}>
               <i className="fa fa-home" aria-hidden="true"></i>
             </RaisedButton>
           </Link>
